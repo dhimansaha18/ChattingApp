5680246b14576340879cb3a13894f8759187649d
dhiman saha <partho.saha01@gmail.com>
2023-06-23 19:47:40 +0600

    'multer-implemented'

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -2,9 +2,14 @@ const express = require("express");
 
 const { getUsers } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
 
 const router = express.Router();
 
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+router.post(
+    "/",
+    avatarUpload);
 
 module.exports = router;
